2c4c165991ed356d82acb839ab4546a35b1bcb69
cuong01 cuong02 <buivuxuancuong@gmail.com>
2022-03-07 17:31:44 +0700

    first commit

---
+++ js/js.js
@@ -0,0 +1,10 @@
+var menu = document.getElementById("menu")
+
+function show(){
+    menu.style.width = "200px";
+    menu.style.display ="block"
+}
+
+function clone(){
+    menu.style.width = "0px"
+}
